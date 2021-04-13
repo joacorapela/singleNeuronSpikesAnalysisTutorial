942716e9863059e74908f4f3d525121014b1f034
Joaquin Rapela <joacorapela@gmail.com>
2021-04-13 12:01:16 +0100

    Added

---
+++ code/scripts/doDecodeInvGaussian_control.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python doDecode.py \
+    --randomize_ISIs \
+    --model_class_name probabilisticModels.InverseGaussian \
+    --fig_filename_pattern ../../figures/decoding_invGaussian_randomized_ISIs{:d}.{:s}
