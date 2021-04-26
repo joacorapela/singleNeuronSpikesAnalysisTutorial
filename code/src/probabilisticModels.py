Fixed but on Exponential logLikelihood pointed by Lucas. Thanks Lucasgit add code/src/probabilisticModels.py!

code/src/probabilisticModels.py
@@ -34,7 +34,7 @@ class Exponential(ProbabilisticModel):
 
     def logLikelihood(self, x):
         N = len(x)
-        answer = N*np.log(self._lambda)*(1-x.mean())
+        answer = N*(np.log(self._lambda)-self._lambda*x.mean())
         return answer
 
 class InverseGaussian(ProbabilisticModel):